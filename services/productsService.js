bf6c6f39b0358eeb2230768f5154d49922567ff4
Nicolas Posa <nicolasposa@Nicolass-MacBook-Air.local>
2023-02-22 09:24:52 -0300

    more services and asyn functions

--- services/productsService.js
+++ services/productsService.js
@@ -16,14 +16,39 @@ class ProductsService {
       });
     }
   }
-  create() {}
-  find() {
+  async create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...data,
+    };
+    this.products.push(newProduct);
+    return newProduct;
+  }
+  async find() {
     return this.products;
   }
-  findOne(id) {
+  async findOne(id) {
     return this.products.find((item) => item.id === id);
   }
-  update() {}
-  delete() {}
+  async update(id, changes) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    const product = this.products[index];
+    this.products[index] = {
+      ...product,
+      ...changes,
+    };
+    return this.products[index];
+  }
+  async delete(id) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
+  }
 }
 module.exports = ProductsService;
