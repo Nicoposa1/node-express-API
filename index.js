010eb49dfb8ec6bdd7e93c673341246c408038fb
Nicolas Posa <nicoposa57@gmail.com>
2023-07-27 19:37:18 -0300

    test

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://${IP}:${port}`)
-);
+); // eslint-disable-line no-console
