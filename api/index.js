2b953dafbea5937f9b7ce461d7715bcaf1dee063
Nicolas Posa <nicoposa57@gmail.com>
2023-08-04 11:27:22 -0300

    fix

--- api/index.js
+++ api/index.js
@@ -9,12 +9,23 @@ const {
   boomErrorHandler,
 } = require('./middleware/error.handler.js');
 
+const IP = os.networkInterfaces().en0[1].address;
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+routerApi(app);
+
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 const whiteList = [
   'http://localhost:8080',
   'https://myapp.co',
   'http://nicoposa.com',
 ];
-
 const options = {
   origin: (origin, callback) => {
     if (whiteList.includes(origin) || !origin) {
@@ -24,24 +35,15 @@ const options = {
     }
   },
 };
-
-const IP = os.networkInterfaces().en0[1].address;
-const app = express();
-
-const port = 3000;
-
-app.use(express.json());
-
-routerApi(app);
-
-app.use(logErrors);
-app.use(boomErrorHandler);
-app.use(errorHandler);
 app.use(cors(options));
 
-app.get('/api', (req, res) => res.send('Hello World!'));
-app.get('/api/nueva-ruta', (req, res) => res.send('Hello this is new route!'));
+app.get('/api', (req, res) => {
+  res.send('Hola mi server en express');
+});
 
+app.get('/api/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
 app.listen(port, () =>
   console.log(`Example app listening at http://${IP}:${port}`)
 );
